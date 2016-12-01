291e5e6c64c32d949aba72d90af14b23dce59a31
Shane Park <sp3558@nyu.edu>
2016-12-01 12:18:24 -0500

    Add files via upload

--- Final/sketch.js
+++ Final/sketch.js
@@ -4,19 +4,32 @@ var letters = [];//to keep track of what letters are on the screen
 var frame = 0;
 var numColumns = 5;
 var fontSize = 270 / numColumns;
+var score = 0;
+var numKeyTyped = 0.0000000000001;//can't divide by 0 
 
 
 function preload(){
-  font = loadFont('data/font0.otf')
+  font = loadFont('data/font0.otf');
 }
 function setup() {
   createCanvas( 300, 600);
+  frameRate(60);
   for( var i = 0; i < numColumns; i++){
-    letters.push([])
+    letters.push([]);
   }
 }
 
 function draw() {
+  for( var i = 0; i < letters.length; i++){
+    if( letters[i].length != 0 && letters[i][letters[i].length - 1].restY < 0){
+      gameover();
+      return;
+    }
+  }
+  if(frame == 3600){
+    youWon();
+    return;
+  }
   background(0);
   for( var i = 0; i < letters.length; i++){//make every letter fall and display
     for( var j = 0; j < letters[i].length; j++){
@@ -24,9 +37,8 @@ function draw() {
       letters[i][j].display();
     }
   }
-  frame += 1
-  if( frame > 90){//repeating action for every 90 frames
-    frame = 0;
+  frame += 1;
+  if( frame % 120 == 0){//repeating action for every 120 frames
     /*
     if(letters.length == 0){
       letters.push(new Letter(0, random(alphabet.split("")), height)); //adding random letter into "letters"
@@ -73,11 +85,26 @@ function keyTyped(){//deleted letters that are types
   for( var i = 0; i < letters.length; i++){
     if( letters[i].length != 0 && key == letters[i][letters[i].length - 1].txt){//compare last letter and whats typed
       letters[i].pop();
+      score += 1;
     }
   }
+  numKeyTyped += 1;
+}
+
+function gameover(){
+  background(0);
+  fill(255,0,0);
+  textFont(font, 20);
+  text( "Game Over", 80, 200);
+  text( "Score: " + score, 100, 230);
+  text( "Accuracy: " + Math.round( score / numKeyTyped * 100 ) + "%", 63, 260);
+}
+
+function youWon(){
+  background(0);
+  fill(255);
+  textFont( font, 20);
+  text( "YOU WON", 90, 300);
 }
 ///difficulty()
-///accuracy(score)
-///no of letters(score)
-///60sec
 ///item
